d89dad7ca8395e9d4a0fd7f302b437f50df5c010
Mohamed Mejri <mejrimed08@gmail.com>
2023-02-13 17:45:26 +0100

    complete reporting.py

--- src/reporting.py
+++ src/reporting.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
+from diagnostics import model_predictions
+from sklearn.metrics import confusion_matrix
 
 
 
@@ -14,7 +16,8 @@ import os
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
 
 
 
@@ -23,7 +26,12 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 def score_model():
     #calculate a confusion matrix using the test data and the deployed model
     #write the confusion matrix to the workspace
-
+    data = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+    y_test = list(data.pop('exited'))
+    preds = model_predictions(data)
+    cf_matrix = confusion_matrix(y_test, preds)
+    sns.heatmap(cf_matrix, annot=True)
+    plt.savefig(os.path.join(dataset_csv_path, 'confusionmatrix.png'))
 
 
 
