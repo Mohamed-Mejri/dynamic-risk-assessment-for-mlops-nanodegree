61da85f207faf216a25687ddf5f4d768dd4c1378
Mohamed Mejri <mejrimed08@gmail.com>
2023-02-13 12:20:38 +0100

    create utils.py

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+"""Module for helpful functions
+
+Author: Mohamed Mejri
+Date: Feb 2023
+"""
+import os
+import shutil
+
+
+def create_dir(path):
+    try:
+        os.mkdir()
+    except FileExistsError:
+        pass
+
+def remove_all_files(path):
+    """ param <path> could either be relative or absolute. """
+    if os.path.isfile(path) or os.path.islink(path):
+        os.remove(path) 
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        raise ValueError("file {} is not a file or dir.".format(path))
